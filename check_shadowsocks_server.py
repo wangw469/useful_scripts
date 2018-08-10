85429d86a0e1cf9eca4dded3825648d696a839c9
xp <wangw469@gmail.com>
2018-08-10 13:47:50 +0800

    update shadowsocks server check script TEST: check server status through haproxy

--- check_shadowsocks_server.py
+++ check_shadowsocks_server.py
@@ -3,10 +3,11 @@ import socket
 import sys
 
 try:
-    print(sys.argv)
-    # h1 = httplib.HTTPConnection('test', 10080, timeout=3)
-    # h1.request("GET", "/")
-    # response = h1.getresponse()
+    # print(sys.argv[3])
+    h1 = httplib.HTTPConnection(sys.argv[3], sys.argv[4], timeout=1)
+    h1.request("GET", "/")
+    response = h1.getresponse()
+    h1.close()
     # print("[PASS]")
 except socket.timeout:
     # print("[FAILED]timeout")
